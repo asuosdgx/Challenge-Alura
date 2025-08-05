c88d6685fc55ebfd22b7ed90d029090bd234a6fd
unknown <dobsfoxx@gmail.com>
2025-08-05 11:16:01 -0300

    Finalização da implementação do sorteio e exibição do nome

---
+++ app.js
@@ -0,0 +1,24 @@
+
+let amigos = [];
+
+function adicionarAmigo() {
+  let input = document.getElementById('amigo');
+  let nome = input.value.trim();
+  }
+  if (nome && !amigos.includes(nome)) {
+    amigos.push(nome);
+    document.getElementById('listaAmigos').innerHTML = amigos.map(a => `<li>${a}</li>`).join('');
+    input.value = '';
+    input.focus();
+  }
+
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    document.getElementById('resultado').innerHTML = '<li>Adicione pelo menos um nome!.</li>';
+    return;
+  }
+  let indice = Math.floor(Math.random() * amigos.length);
+  let sorteado = amigos[indice];
+  document.getElementById('resultado').innerHTML = `<li>Sorteado: ${sorteado}</li>`;
+}
